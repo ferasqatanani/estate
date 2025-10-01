912533ddcb727f2600026a498ddd53c1b775fd12
Feras Qatanani <187905897+ferasqatanani@users.noreply.github.com.>
2025-10-01 23:27:47 +0300

    Initial commit: Estate module (tutorial app)

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Estate",
+    "version": "18.0.1.0.0",
+    "summary": "Real estate listings (tutorial app)",
+    "author": "Feras",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "depends": ["base"],
+    "data": [
+    "views/estate_views.xml",],
+    "application": True,
+    "installable": True
+}
---
+++ estate/models/property.py
@@ -0,0 +1,27 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Has Garage?")
+    garden = fields.Boolean(string="Has Garden?")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection(
+        string="Garden Orientation",
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ],
+    )
