5e02627217854dfc7e0bc5ff0a300e5d2d88f61f
Feras Qatanani <187905897+ferasqatanani@users.noreply.github.com.>
2025-10-02 15:35:27 +0300

    Remove duplicate model; import full model (property.py)

--- estate/models/estate_property.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-class EstateProperty(models.Model):
-    _name = "estate.property"
-    _description = "Estate Property (minimal)"
-
-    name = fields.Char(required=True)
-    expected_price = fields.Float()
